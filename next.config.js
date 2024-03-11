faa865daebe44237664880848c962c0311a43534
QuocAnh189 <146860044+QuocAnh189@users.noreply.github.com>
2024-03-11 20:53:56 +0700

    config overview app (#1)

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    BASE_API_URL: process.env.BASE_API_URL,
+    FILE_API_URL: process.env.FILE_API_URL,
+    AUTH_API_URL: process.env.AUTH_API_URL
+    // TICKET_API_URL: process.env.TICKET_API_URL,
+    // EVENT_API_URL: process.env.EVENT_API_URL,
+    // CATEGORY_API_URL: process.env.CATEGORY_API_URL,
+    // PAYMENT_API_URL: process.env.PAYMENT_API_URL
+  },
+  images: {
+    domains: [
+      'dummyimage.com',
+      'picsum.photos',
+      'tickettvdstorage.blob.core.windows.net',
+      'tickettvdblobstorage.blob.core.windows.net'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+        port: '',
+        pathname: '/my-bucket/**'
+      }
+    ]
+  },
+  compiler: {
+    styledComponents: true
+  }
+};
+
+module.exports = nextConfig;
