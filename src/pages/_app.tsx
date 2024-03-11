6d1188259acb9fa2fe8bf175daac37523586afda
QuocAnh189 <anhquoc18092003@gmail.com>
2024-03-11 20:52:16 +0700

    config overview app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "src/styles/globals.css";
-import type { AppProps } from "next/app";
+import 'src/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Event Hub</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
